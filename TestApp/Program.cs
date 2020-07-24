13534460013098a688ee484fa17c20572f7f97c8
Andrew Salko <a.salko@foss.ua>
2020-07-24 17:09:05 +0300

    Load xml sitemap to mongo test db

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -17,9 +17,16 @@ namespace TestApp
 				HttpClient httpClient = new HttpClient();
 
 				kawaii.twitter.core.SiteMap.XMLSiteMapLoader loader = new kawaii.twitter.core.SiteMap.XMLSiteMapLoader(httpClient);
-				loader.LimitCount = 4;
+				//loader.LimitCount = 4;
 
 				kawaii.twitter.core.SiteMap.URLInfo[] resultURLs = await loader.Load(postSiteMapURL);
+
+				if (resultURLs != null && resultURLs.Length > 0)
+				{
+
+					//теперь заливаем в базу
+					await _LoadToMongo(resultURLs);
+				}
 
 			}
 			catch (Exception ex)
@@ -30,18 +37,31 @@ namespace TestApp
 			Console.WriteLine("Done");
 		}
 
-		static async Task _LoadToMongo()
+		static async Task _LoadToMongo(kawaii.twitter.core.SiteMap.URLInfo[] urls)
 		{
 			string mongoConnectionString = "mongodb://localhost:27017/?readPreference=primary&appname=kawaiitwitter&ssl=false";
 
 			kawaii.twitter.db.Database database = new kawaii.twitter.db.Database(mongoConnectionString, false);
 
-			kawaii.twitter.db.SitePage p1 = new kawaii.twitter.db.SitePage();
-			p1.Title = "t1";
-			p1.URL = "https://test1";
-			p1.LastModified = new DateTime(2020, 04, 26);
+			var sitePages = database.SitePages;
 
-			await database.SitePages.InsertOneAsync(p1);
+			foreach (var url in urls)
+			{
+				kawaii.twitter.db.SitePage page = new kawaii.twitter.db.SitePage
+				{
+					LastModified = url.LastModified,
+					Title = url.Title,
+					URL = url.URL
+				};
+
+				await sitePages.InsertOneAsync(page);
+			}
+
+			//kawaii.twitter.db.SitePage p1 = new kawaii.twitter.db.SitePage();
+			//p1.Title = "t1";
+			//p1.URL = "https://test1";
+			//p1.LastModified = new DateTime(2020, 04, 26);
+			//await database.SitePages.InsertOneAsync(p1);
 		}
 
 		static async Task _TestTwitterImage()
